788d67ca84c1cf466f752be31a8ebdfe2f59f8cc
JVincent Garret <vincentgarret33@gmail.com>
2021-07-15 17:59:34 +0200

    header & 1st section

---
+++ src/Controller/Front/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Controller\Front;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home(){
+        return $this->render('front/home.html.twig');
+    }
+}
