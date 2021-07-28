a9d5d07899a592705d459ff3fe07b6a8e0e48f68
JVincent Garret <vincentgarret33@gmail.com>
2021-07-28 10:52:47 +0200

    footer

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -4,6 +4,11 @@
 namespace App\Controller\Front;
 
 
+use App\Entity\Contact;
+use App\Form\ContactFormType;
+use App\Repository\ContactRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,7 +17,21 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function home(){
-        return $this->render('front/home.html.twig');
+    public function home(Request $request,
+                            EntityManagerInterface $entityManager,
+                            ContactRepository $contactRepository)
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactFormType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $entityManager->persist($contact);
+            $entityManager->flush();
+            $this->addFlash('succes','Votre demande à bine été envoyé');
+        }
+        return $this->render('front/home.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
